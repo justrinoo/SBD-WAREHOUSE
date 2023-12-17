24cf2ba9c72bfec22f3c83bc4b32ea74ff7c6391
raymond <antisocialraymond@gmail.com>
2023-12-17 15:00:47 +0700

    chore : yay or nay

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({
-        secret: 'warehouse-app',
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false, maxAge: null },
-    })
+	session({
+		secret: 'warehouse-app',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false, maxAge: null },
+	}),
 );
 
 app.use('/', indexRouter);
@@ -45,28 +45,40 @@ app.use('/', stokRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Atau '*'
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+	res.header(
+		'Access-Control-Allow-Origin',
+		'http://localhost:3000',
+	); // Atau '*'
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, PUT, POST, DELETE',
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Content-Type',
+	);
+	next();
 });
 
 // Get your endpoint online
 ngrok
-    .connect({ addr: 3000, authtoken_from_env: true })
-    .then((listener) =>
-        console.log(`Ingress established at: ${listener.url()}`)
-    );
+	.connect({ addr: 3000, authtoken_from_env: true })
+	.then((listener) =>
+		console.log(
+			`Ingress established at: ${listener.url()}`,
+		),
+	);
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error =
+		req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
